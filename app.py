16fce4d4c820c7831884eba1843293b109aa7dcd
Rishu gurjar <gurjarrishu547@gmail.com>
2026-07-07 18:54:00 +0530

    Fix indentation for exception handling

--- app.py
+++ app.py
@@ -402,7 +402,7 @@ else:
             )
 
 
-            except Exception as e:
+        except Exception as e:
                 st.error(f"Prediction Error : {e}")
 
 # ==========================================
